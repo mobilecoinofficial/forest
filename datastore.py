724410262e950c5e883e96a1ec46d3280431a4fa
technillogue <technillogue@gmail.com>
2021-08-13 19:02:10 -0400

    check for groups before anything else. fix psql constraints. fix sigcli to send group info correctly

--- datastore.py
+++ datastore.py
@@ -146,7 +146,7 @@ class SignalDatastore:
         buffer = BytesIO(record[0].get("datastore"))
         tarball = TarFile(fileobj=buffer)
         fnames = [member.name for member in tarball.getmembers()]
-        logging.debug(fnames)
+        logging.debug(fnames[:2])
         logging.info(
             "expected file %s exists: %s",
             self.filepath,
@@ -180,7 +180,7 @@ class SignalDatastore:
             )
             tarball.add("data")
         fnames = [member.name for member in tarball.getmembers()]
-        logging.debug(fnames)
+        logging.debug(fnames[:2])
         tarball.close()
         buffer.seek(0)
         data = buffer.read()
