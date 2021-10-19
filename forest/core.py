654985f89bfd0ebb59a0a9e18687a55f8caf9582
technillogue <technillogue@gmail.com>
2021-10-19 01:15:10 -0400

    refactor datastore into autosave and add a datastore-specific dockerfile as an example

--- forest/core.py
+++ forest/core.py
@@ -18,9 +18,9 @@ from phonenumbers import NumberParseException
 
 # framework
 from forest import datastore
+from forest import autosave
 from forest import pghelp
 from forest import utils
-
 JSON = dict[str, Any]
 Response = Union[str, list, dict[str, str], None]
 
@@ -232,8 +232,8 @@ class Signal:
         await self.datastore.mark_freed()
         await pghelp.close_pools()
         # this doesn't work. see https://github.com/forestcontact/forest-draft/issues/10
-        if datastore._memfs_process:
-            executor = datastore._memfs_process._get_executor()
+        if autosave._memfs_process:
+            executor = autosave._memfs_process._get_executor()
             logging.info(executor)
             executor.shutdown(wait=False, cancel_futures=True)
         logging.info("exited".center(60, "="))
@@ -365,8 +365,8 @@ app.add_routes(
 )
 
 if not utils.get_secret("NO_MEMFS"):
-    app.on_startup.append(datastore.start_memfs)
-    app.on_startup.append(datastore.start_memfs_monitor)
+    app.on_startup.append(autosave.start_memfs)
+    app.on_startup.append(autosave.start_memfs_monitor)
 
 
 if __name__ == "__main__":
--- forest/datastore.py
+++ forest/datastore.py
@@ -1,25 +1,32 @@
 #!/bin/python3.9
+import argparse
 import asyncio
 import json
 import logging
 import os
 import shutil
+import sys
 import time
 from io import BytesIO
 from pathlib import Path
-from subprocess import PIPE, Popen
 from tarfile import TarFile
-from typing import Any, Optional, Callable, cast
-import argparse
+from typing import Any, Callable, Optional, cast
 
-import aioprocessing
-from aiohttp import web
 
-from forest import utils
-from forest import fuse
-from forest import mem
-from forest.pghelp import PGExpressions, PGInterface
-
+try:
+    # normally in a package
+    from forest import pghelp, utils
+except ImportError:
+    # maybe we're local?
+    try:
+        import pghelp
+        import utils
+    except ImportError:
+        # i wasn't asking
+        sys.path.append("forest")
+        sys.path.append("..")
+        import pghelp  # pylint: disable=ungrouped-imports
+        import utils  # pylint: disable=ungrouped-imports
 if utils.get_secret("MIGRATE"):
     get_datastore = "SELECT account, datastore FROM {self.table} WHERE id=$1"
 else:
@@ -30,7 +37,7 @@ class DatastoreError(Exception):
     pass
 
 
-AccountPGExpressions = PGExpressions(
+AccountPGExpressions = pghelp.PGExpressions(
     table="signal_accounts",
     # rename="ALTAR TABLE IF EXISTS prod_users RENAME TO {self.table}",
     migrate="ALTER TABLE IF EXISTS {self.table} ADD IF NOT EXISTS datastore BYTEA, notes TEXT",
@@ -65,8 +72,8 @@ AccountPGExpressions = PGExpressions(
 )
 
 
-def get_account_interface() -> PGInterface:
-    return PGInterface(
+def get_account_interface() -> pghelp.PGInterface:
+    return pghelp.PGInterface(
         query_strings=AccountPGExpressions,
         database=utils.get_secret("DATABASE_URL"),
     )
@@ -206,6 +213,30 @@ class SignalDatastore:
         return await self.account_interface.mark_account_freed(self.number)
 
 
+def setup_tmpdir() -> None:
+    if not utils.LOCAL:
+        return
+    if utils.ROOT_DIR == ".":
+        logging.warning("not setting up tmpdir")
+        return
+    if utils.ROOT_DIR == "/tmp/local-signal/":
+        try:
+            shutil.rmtree(utils.ROOT_DIR)
+        except (FileNotFoundError, OSError) as e:
+            logging.warning("couldn't remove rootdir: %s", e)
+    (Path(utils.ROOT_DIR) / "data").mkdir(exist_ok=True)
+    # assume we're running in the repo
+    sigcli = utils.get_secret("SIGNAL_CLI_PATH") or "signal-cli"
+    sigcli_path = Path(sigcli).absolute()
+    logging.info("symlinking %s to %s", sigcli_path, utils.ROOT_DIR)
+    os.symlink(sigcli_path, utils.ROOT_DIR + "/signal-cli")
+    os.symlink(Path("avatar.png").absolute(), utils.ROOT_DIR + "/avatar.png")
+    logging.info("chdir to %s", utils.ROOT_DIR)
+    os.chdir(utils.ROOT_DIR)
+    logging.info("not starting memfs because running locally")
+    return
+
+
 async def getFreeSignalDatastore() -> SignalDatastore:
     interface = get_account_interface()
     await interface.free_accounts_not_updated_in_the_last_hour()
@@ -213,119 +244,15 @@ async def getFreeSignalDatastore() -> SignalDatastore:
     if not record:
         raise Exception("no free accounts")
         # alternatively, register an account...
+        # could put some of register.py/signalcaptcha handler here...
     number = record[0].get("id")
     logging.info(number)
     assert number
     return SignalDatastore(number)
 
 
-_memfs_process = None
-
-
-async def start_memfs(app: web.Application) -> None:
-    """
-    mount a filesystem in userspace to store data
-    the fs contents are stored in memory, so that our keys never touch a disk
-    this means we can log signal-cli's interactions with fs,
-    and store them in mem_queue.
-    if running locally, chdir to /tmp/local-signal with symlinks instead
-    """
-    # refactor this whole mess into some sort of more general "figure out where we are before downloading"
-    if utils.LOCAL:
-        if utils.ROOT_DIR == ".":
-            logging.warning("not deleting current dir, so not starting memfs")
-            return
-        try:
-            shutil.rmtree(utils.ROOT_DIR)
-        except (FileNotFoundError, OSError) as e:
-            logging.warning("couldn't remove rootdir: %s", e)
-        os.mkdir(utils.ROOT_DIR)
-        os.mkdir(utils.ROOT_DIR + "/data")
-        # we're going to be running in the repo
-        sigcli = utils.get_secret("SIGNAL_CLI_PATH") or "signal-cli"
-        sigcli_path = Path(sigcli).absolute()
-        logging.info("symlinking %s to %s", sigcli_path, utils.ROOT_DIR)
-        os.symlink(sigcli_path, utils.ROOT_DIR + "/signal-cli")
-        os.symlink(Path("avatar.png").absolute(), utils.ROOT_DIR + "/avatar.png")
-        logging.info("chdir to %s", utils.ROOT_DIR)
-        os.chdir(utils.ROOT_DIR)
-        logging.info("not starting memfs because running locally")
-        return
-    logging.info("starting memfs")
-    app["mem_queue"] = mem_queue = aioprocessing.AioQueue()
-    if not os.path.exists("/dev/fuse"):
-        # you *must* have fuse already loaded if running locally
-        proc = Popen(
-            ["/usr/sbin/insmod", "/app/fuse.ko"],
-            stdout=PIPE,
-            stderr=PIPE,
-        )
-        proc.wait()
-        (stdout, stderr) = proc.communicate()  # pylint: disable=unused-variable
-        if stderr:
-            raise Exception(
-                f"Could not load fuse module! You may need to recompile.\t\n{stderr.decode()}"
-            )
-
-    def memfs_proc(path: str = "data") -> Any:
-        """Start the memfs process"""
-        pid = os.getpid()
-        open("/dev/stdout", "w").write(
-            f"Starting memfs with PID: {pid} on dir: {path}\n"
-        )
-        backend = mem.Memory(logqueue=mem_queue)  # type: ignore
-        logging.info("initing FUSE")
-        return fuse.FUSE(operations=backend, mountpoint=utils.ROOT_DIR + "/data")  # type: ignore
-
-    async def launch() -> None:
-        logging.info("about to launch memfs with aioprocessing")
-        memfs = aioprocessing.AioProcess(target=memfs_proc)
-        memfs.start()  # pylint: disable=no-member
-        app["memfs"] = memfs
-        _memfs_process = memfs
-
-    logging.info("awaiting launch func")
-    await launch()
-
-
-# input, operation, path, arguments, caller
-# ["->", "fsync", "/+14703226669", "(1, 2)", "/app/signal-cli", ["/app/signal-cli", "--config", "/app", "--username=+14703226669", "--output=json", "stdio", ""], 0, 0, 523]
-# ["<-", "fsync", "0"]
-async def start_memfs_monitor(app: web.Application) -> None:
-    """
-    monitor the memfs activity queue for file saves, sync with supabase
-    """
-
-    async def upload_after_signalcli_writes() -> None:
-        queue = app.get("mem_queue")
-        if not queue:
-            logging.info("no mem_queue, nothing to monitor")
-            return
-        logging.info("monitoring memfs")
-        counter = 0
-        while True:
-            queue_item = await queue.coro_get()
-            # iff fsync triggered by signal-cli
-            if (
-                queue_item[0:2] == ["->", "fsync"]
-                and queue_item[5][0] == utils.ROOT_DIR + "/signal-cli"
-            ):
-                # /+14703226669
-                # file_to_sync = queue_item[2]
-                # 14703226669
-                maybe_session = app.get("session")
-                if maybe_session:
-                    counter += 1
-                    if time.time() % (60 * 3) == 0:
-                        logging.info("background syncs in the past ~3min: %s", counter)
-                        counter = 0
-                    await maybe_session.datastore.upload()
-
-    app["mem_task"] = asyncio.create_task(upload_after_signalcli_writes())
-
-
 # this stuff desperately needs to be cleaned up and probably moved to scripts
-# maybe a config about where we're running
+# maybe a config about where we're running:
 # MEMFS, DOWNLOAD, ROOT_DIR, HOSTNAME, etc
 # is HCL overkill?
 
@@ -360,9 +287,10 @@ def subcommand(
     Then on the command line::
         $ python cli.py subcommand -d
     """
+
     def decorator(func: Callable) -> None:
         _parser = parent.add_parser(func.__name__, description=func.__doc__)
-        for arg in (_args if _args else []):
+        for arg in _args if _args else []:
             _parser.add_argument(*arg[0], **arg[1])
         _parser.set_defaults(func=func)
 
@@ -395,9 +323,9 @@ async def do_free(ns: argparse.Namespace) -> None:
 
 @subcommand([argument("--number")])
 async def sync(ns: argparse.Namespace) -> None:
-    app = cast(web.Application, {})
-    asyncio.create_task(start_memfs(app))
-    await start_memfs_monitor(app)
+    setup_tmpdir() 
+    #asyncio.create_task(start_memfs(app))  # ???
+    #await start_memfs_monitor(app)
     try:
         datastore = SignalDatastore(ns.number)
         await datastore.download()
