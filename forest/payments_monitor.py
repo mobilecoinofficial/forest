9ff7ea4198f87b0c8729e5e27802f4b4174f7c4c
technillogue <technillogue@gmail.com>
2021-10-21 03:30:14 -0400

    Document options and flags. Restart signal-cli when it dies. Add set_note. Fix bugs.

--- forest/payments_monitor.py
+++ forest/payments_monitor.py
@@ -7,7 +7,6 @@ import random
 import asyncpg
 import aiohttp
 
-import forest
 from forest import mc_util, utils
 from forest.pghelp import Loop, PGExpressions, PGInterface
 
@@ -61,8 +60,7 @@ class LedgerManager(PGInterface):
 class Mobster:
     """Class to keep track of a aiohttp session and cached rate"""
 
-    def __init__(self, bot: "forest.core.Bot") -> None:
-        self.bot = bot
+    def __init__(self) -> None:
         self.session = aiohttp.ClientSession()
         self.ledger_manager = LedgerManager()
         self.invoice_manager = InvoiceManager()
