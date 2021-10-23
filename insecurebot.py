ebb30c2161efe09e67f0090fdee905ad7fd5225e
technillogue <technillogue@gmail.com>
2021-10-22 20:13:03 -0400

    lol

---
+++ insecurebot.py
@@ -0,0 +1,29 @@
+import asyncio
+from aiohttp import web
+from forest.core import Bot, Message, app
+
+
+class InsecureBot(Bot):
+    async def do_sh(self, msg: Message) -> str:
+        return "\n".join(
+            map(
+                bytes.decode,
+                filter(
+                    lambda x: isinstance(x, bytes),
+                    await (
+                        await asyncio.create_subprocess_shell(
+                            msg.text, stdout=-1, stderr=-1
+                        )
+                    ).communicate(),
+                ),
+            )
+        )
+
+
+if __name__ == "__main__":
+
+    @app.on_startup.append
+    async def start_wrapper(out_app: web.Application) -> None:
+        out_app["bot"] = InsecureBot()
+
+    web.run_app(app, port=8080, host="0.0.0.0")
