d8f60ae9fbbdaa869bfad19666072c5bb5544dca
technillogue <technillogue@gmail.com>
2021-11-05 22:03:00 -0400

    suppress receipts, bad task

--- forest/core.py
+++ forest/core.py
@@ -164,12 +164,14 @@ class Signal:
         try:
             if "params" in blob and isinstance(blob["params"], list):
                 for msg in blob["params"]:
-                    await self.auxincli_output_queue.put(AuxinMessage(msg))
+                    if not blob.get("content", {}).get("receipt_message", {}):
+                        await self.auxincli_output_queue.put(AuxinMessage(msg))
                 return
             if "result" in blob:
                 if isinstance(blob.get("result"), list):
                     for msg in blob.get("result"):
-                        await self.auxincli_output_queue.put(AuxinMessage(msg))
+                        if not blob.get("content", {}).get("receipt_message", {}):
+                            await self.auxincli_output_queue.put(auxinmessage(msg))
                     return
                 msg = AuxinMessage(blob)
                 await self.auxincli_output_queue.put(msg)
@@ -374,12 +376,15 @@ class Bot(Signal):
     async def handle_messages(self) -> None:
         """Read messages from the queue and pass each message to handle_message
         If that returns a non-empty string, send it as a response"""
+        pending_tasks = []
         async for message in self.auxincli_output_iter():
             # potentially stick a try-catch block here and send errors to admin
-            print(message)
-            response = await self.handle_message(message)
-            if response:
-                await self.respond(message, response)
+            print(f"doing work with {message}")
+            async def do_work():
+                response = await self.handle_message(message)
+                if response is not None:
+                    await self.respond(message, response)
+            pending_tasks.append(asyncio.create_task(do_work()))
 
     async def handle_message(self, message: Message) -> Response:
         """Method dispatch to do_x commands and goodies.
@@ -455,6 +460,18 @@ class Bot(Signal):
                 "did you include the country code?",
             )
             return None
+
+    async def do_address(self, message: Message) -> str:
+        recipient = message.source
+        result = await self.auxin_req("getPayAddress", peer_name=recipient)
+        b64_address = (
+            result.blob.get("Address", {}).get("mobileCoinAddress", {}).get("address")
+        )
+        if result.error or not b64_address:
+            logging.info("bad address: %s", result.blob)
+            return "sorry, couldn't get your MobileCoin address"
+        address = mc_util.b64_public_address_to_b58_wrapper(b64_address)
+        return address
 
     async def handle_payment(self, message: Message) -> None:
         """Decode the receipt, then update balances.
