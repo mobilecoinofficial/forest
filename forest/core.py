0949da97407c33a24c0a79cba0fea2d0d4ecad92
itdaniher <itdaniher@gmail.com>
2021-12-05 14:18:27 -0500

    + cash code implementation (#40)
    cash code implementation - funding is a little weird, as funds are expected to be in the full service account already.
    
    Co-authored-by: infra <infra@sterile.solutions>
    Co-authored-by: technillogue <technillogue@gmail.com>

--- forest/core.py
+++ forest/core.py
@@ -43,6 +43,7 @@ roundtrip_summary = Summary("roundtrip_s", "Roundtrip message response time")
 
 MessageParser = AuxinMessage if utils.AUXIN else StdioMessage
 logging.info("Using message parser: %s", MessageParser)
+fee_pmob = int(1e12 * 0.0004)
 
 
 def rpc(
@@ -675,6 +676,32 @@ class PayBot(Bot):
         content_skeletor["dataMessage"]["body"] = None
         content_skeletor["dataMessage"]["payment"] = payment
         return json.dumps(content_skeletor)
+
+    async def build_cash_code(
+        self, recipient: str, amount_pmob: int
+    ) -> Optional[Message]:
+        """Builds a cash code and sends to a recipient, given a recipient as phone number and amount in pMOB."""
+        raw_prop = await self.mob_request(
+            "build_gift_code",
+            account_id=await self.mobster.get_account(),
+            value_pmob=str(int(amount_pmob)),
+            fee=str(fee_pmob),
+            memo="Cash code built with MOBot!",
+        )
+        prop = raw_prop["result"]["tx_proposal"]
+        b58_code = raw_prop["result"]["gift_code_b58"]
+        submitted = await self.mob_request(
+            "submit_gift_code",
+            tx_proposal=prop,
+            gift_code_b58=b58_code,
+            from_account_id=await self.mobster.get_account(),
+        )
+        b58 = submitted.get("result", {}).get("gift_code", {}).get("gift_code_b58")
+        await self.send_message(
+            recipient,
+            f"Built Cash Code {b58} redeemable for {str(mc_util.pmob2mob(amount_pmob-fee_pmob)).rstrip('0')} MOB",
+        )
+        return None
 
     async def send_payment(
         self, recipient: str, amount_pmob: int, receipt_message: str = "receipt sent!"
