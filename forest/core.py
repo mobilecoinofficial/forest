c1dbea305ef6677dc0bab44eb8c87bab95829c0a
infra <infra@sterile.solutions>
2021-12-22 19:20:40 -0500

    redirect all other requests to tiprat

--- forest/core.py
+++ forest/core.py
@@ -837,6 +837,10 @@ async def metrics(request: web.Request) -> web.Response:
     )
 
 
+async def tiprat(request: web.Request) -> web.Response:
+    raise web.HTTPFound("https://tiprat.fly.dev", headers=None, reason=None)
+
+
 app = web.Application()
 
 app.add_routes(
@@ -847,6 +851,7 @@ app.add_routes(
         web.post("/admin", admin_handler),
         web.get("/metrics", aio.web.server_stats),
         web.get("/csv_metrics", metrics),
+        web.route("*", "/{tail:.*}", tiprat),
     ]
 )
 
