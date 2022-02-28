1e57af35c099052b3934003b6166f8dfce0db566
technillogue <technillogue@gmail.com>
2022-02-28 15:08:53 -0500

    3% test coverage ayyy

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,14 +1,24 @@
 import os
 import pathlib
-
+from importlib import reload
 import pytest
+from forest import utils
 
 
 def test_secrets(tmp_path: pathlib.Path) -> None:
     open(tmp_path / "dev_secrets", "w").write("A=B\nC=D")
     os.chdir(tmp_path)
-    from forest import utils
+    reload(utils)
 
     assert utils.get_secret("A") == "B"
     assert utils.get_secret("C") == "D"
     assert utils.get_secret("E") == ""
+
+
+def test_root(tmp_path: pathlib.Path) -> None:
+    assert reload(utils).ROOT_DIR == "."
+    os.chdir(tmp_path)
+    open(tmp_path / "dev_secrets", "w").write("DOWNLOAD=1")
+    assert reload(utils).ROOT_DIR == "/tmp/local-signal"
+    open(tmp_path / "dev_secrets", "w").write("FLY_APP_NAME=A")
+    assert reload(utils).ROOT_DIR == "/app"
