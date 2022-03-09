8eaba48d0e2c8d55b50ba3a601b4eaf3e0ac4483
deepfates <deepfates@gmail.com>
2022-03-09 17:00:36 -0600

    cleanup DialogBot methods

--- dialogbot.py
+++ dialogbot.py
@@ -42,26 +42,6 @@ class DialogBot(Bot):
             return response.query_result.fulfillment_text
         return await self.default(message)
 
-    async def do_template(self, _: Message) -> str:
-        """
-        A template you can fill in to make your own bot. Anything after do_ is a / command.
-        Return value is used to send a message to the user.
-        """
-        return "template."
-
-    async def do_hello(self, _: Message) -> str:
-        """
-        Simple, Hello, world program. Type /hello and the bot will say "Hello, world!"
-
-        """
-        return "Hello, world!"
-
-    async def do_echo(self, message: Message) -> str:
-        """
-        Repeats what you said. Type /echo foo and the bot will say "foo".
-        """
-        return message.text
-
 
 if __name__ == "__main__":
     run_bot(DialogBot)
