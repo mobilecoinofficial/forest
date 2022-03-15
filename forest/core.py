76a1124e1efc56279b8e01f8fe814e000debfffa
technillogue <technillogue@gmail.com>
2022-03-15 18:50:42 -0400

    don't enqueue PING ids

--- forest/core.py
+++ forest/core.py
@@ -273,6 +273,8 @@ class Signal:
     async def enqueue_blob_messages(self, blob: JSON) -> None:
         "turn rpc blobs into the appropriate number of Messages and put them in the inbox"
         message_blob: Optional[JSON] = None
+        if blob["id"] == "PONG":
+            return None
         logging.info(json.dumps(blob))
         if "params" in blob:
             if isinstance(blob["params"], list):
