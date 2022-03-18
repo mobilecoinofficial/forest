d29d6c95164323c128e684d92eb80d802a873e2a
cxloe <cxl0e@protonmail.com>
2022-03-17 20:13:16 -0400

    add unknown address handling

--- tests/testbots/test_questions_bot.py
+++ tests/testbots/test_questions_bot.py
@@ -120,10 +120,12 @@ class TestBot(QuestionBot):
     async def do_test_address_question(self, message: Message) -> Response:
         """Asks a sample address question"""
 
-        address = await self.ask_address_question(message.source)
-
+        try:
+            address = await self.ask_address_question(message.source)
+        except:
+            return "Can't find your address! Try specifying your state or zip code."
         return address
-
+#
 
 if __name__ == "__main__":
     run_bot(TestBot)
