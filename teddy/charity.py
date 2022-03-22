76fb0b78c264557ada6cf240e0a08f77fe7ed714
infra <infra@sterile.solutions>
2022-03-21 22:07:39 -0400

    breakout yes/no defaults, add rewards / levels / fulfillment info, lowercase multiple choice if possible

--- teddy/charity.py
+++ teddy/charity.py
@@ -127,6 +127,8 @@ class Charity(TalkBack):
         self.easter_eggs: aPersistDict[str] = aPersistDict("easter_eggs")
         self.first_messages = aPersistDictOfInts("first_messages")
         self.donations: aPersistDict[str] = aPersistDict("donations")
+        self.donation_rewards: aPersistDict[str] = aPersistDict("donation_rewards")
+        self.reward_levels: aPersistDict[int] = aPersistDict("reward_levels")
         self.user_sessions: aPersistDictOfLists[str] = aPersistDictOfLists(
             "user_sessions"
         )
@@ -187,6 +189,61 @@ class Charity(TalkBack):
             await self.user_sessions.extend(message.uuid, message.full_text)
         return await super().handle_message(message)
 
+    @hide
+    async def do_fulfillment(self, msg):
+        return await self.donation_rewards.get(await self.fulfillment(msg))
+
+    async def fulfillment(self, msg: Message, donation_uid: str = get_uid()):
+        user = msg.uuid
+        await self.send_message(
+            user,
+            await self.dialog.get("THANK_YOU_WE_WILL_SHIP", "THANK_YOU_WE_WILL_SHIP"),
+        )
+        delivery_name = (await self.get_displayname(msg.uuid)).split("_")[0]
+        if not await self.ask_yesno_question(
+            user,
+            f"Should we address your package to {delivery_name}?",
+        ):
+            delivery_name = await self.ask_freeform_question(
+                user, "To what name should we address your package?"
+            )
+        delivery_address = await self.ask_address_question(
+            user, require_confirmation=True
+        )
+        merchandise_size = await self.ask_multiple_choice_question(
+            user,
+            "What size shirt do you wear?",
+            options={
+                "XS": "Extra Small",
+                "S": "Small",
+                "M": "Medium",
+                "L": "Large",
+                "XL": "Extra Large",
+            },
+        )
+        if await self.ask_yesno_question(
+            user,
+            "Would you like to provide an email address in case we need to contact you about your order? Otherwise, we will message you on Signal!",
+        ):
+            user_email = await self.ask_freeform_question(user, "What's your email?")
+        else:
+            user_email = None
+        if not msg.source or await self.ask_yesno_question(
+            user,
+            "Would you like to provide an alternate phone number for your package? Otherwise, we will use the one you've registered on Signal!",
+        ):
+            user_phone = await self.ask_freeform_question(
+                user, "What's your phone number?"
+            )
+        else:
+            user_phone = msg.source
+        await self.donation_rewards.set(
+            donation_uid,
+            f'{delivery_name}, "{delivery_address}", {merchandise_size}, {user_email}, {user_phone}',
+        )
+        await self.send_message(user, await self.dialog.get("GOT_IT", "GOT_IT"))
+        return donation_uid
+
     async def default(self, message: Message) -> Response:
         # pylint: disable=too-many-return-statements,too-many-branches
         msg = message
@@ -216,16 +273,25 @@ class Charity(TalkBack):
 
     @time_(REQUEST_TIME)
     async def payment_response(self, msg: Message, amount_pmob: int) -> Response:
+        user = msg.uuid
         amount_mob = float(pmob2mob(amount_pmob).quantize(Decimal("1.0000")))
         amount_mmob = int(amount_mob * 1000)
         donation_uid = get_uid()
         donation_time = time.time()
         code = await self.dialog.get("CHARITY", "CHARITY")
         await self.donations.set(
-            donation_uid, f"{msg.uuid}, {donation_time}, {amount_mob}, {code}"
+            donation_uid, f"{user}, {donation_time}, {amount_mob}, {code}"
         )
         await self.charities_balance_mmob.increment(code, amount_mmob)
-        return await self.dialog.get("THANK_YOU_FOR_DONATION", "THANK_YOU_FOR_DONATION")
+        if amount_mmob > await self.reward_levels.get(f"{code}_ship", 10_000):
+            await self.fulfillment(msg, donation_uid)
+            return None
+        elif amount_mmob > await self.reward_levels.get(f"{code}_download", 10_000):
+            return await self.dialog.get("THANK_YOU_PLEASE_DL", "THANK_YOU_PLEASE_DL")
+        else:
+            return await self.dialog.get(
+                "THANK_YOU_FOR_DONATION", "THANK_YOU_FOR_DONATION"
+            )
 
 
 if __name__ == "__main__":
