a961f618da58e03e271a7ade323c2e0a244f080b
deepfates <deepfates@gmail.com>
2022-03-22 12:13:22 -0600

    respond to mentions or quotes

--- imposter/imposter.py
+++ imposter/imposter.py
@@ -25,22 +25,29 @@ class Imposter(Bot):
         self.agent = Agent.from_json(config_file)
         super().__init__()
 
+    def quotes_us(self, msg: Message) -> bool:
+        return msg.quote.author == self.bot_number or msg.quote.author
+
     def match_command(self, msg: Message) -> str:
         if not msg.arg0:
             return ""
         # Look for direct match before checking activators
         if hasattr(self, "do_" + msg.arg0):
             return msg.arg0
-        # If we're not in a group we can just respond to the message
-        if msg.full_text and not msg.group:
-            return "respond"
-        # Try activators
-        if any(o in msg.full_text for o in self.agent.activators):
-            return "respond"
+        # If we're not in a group we can just respond to the message,
+        # Otherwise try activators
+        if (
+            (msg.full_text and not msg.group)
+            or self.mentions_us(msg)
+            or self.quotes_us(msg)
+            or any(o in msg.full_text for o in self.agent.activators)
+        ):
+            return "generate_response"
         # Pass the buck
         return super().match_command(msg)
 
-    async def do_respond(self, msg: Message) -> str:
+    async def do_generate_response(self, msg: Message) -> str:
+        # Send a typing indicator in case the generator takes a while
         await self.outbox.put(rpc("sendTyping", recipient=[msg.source]))
         reply = await self.agent.generate_agent_response(msg.full_text)
         await self.outbox.put(rpc("sendTyping", recipient=[msg.source], stop=True))
