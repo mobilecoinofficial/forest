ccf262187ad22de05471ce96d7f44c0d99b1e851
transfaeries <transfaeries@gmail.com>
2022-03-28 16:00:09 -0400

    free commit

--- schedule.py
+++ schedule.py
@@ -10,29 +10,36 @@ import maya
 from forest.core import QuestionBot, Message, run_bot
 
 
-
 class ScheduleBot(QuestionBot):
     """A bot that lets you schedule when to send a message"""
 
     # async def __init__(self, bot_number: Optional[str] = None) -> None:
-        
-        
+
     #     asyncio.create_task(self.midnight_job(self.report))
     #     super().__init__(bot_number)
 
-    
-
-    async def schedule_send_message(self, recipient, outgoing_message, outgoing_time) -> None:
+    async def schedule_send_message(
+        self, recipient, outgoing_message, outgoing_time
+    ) -> None:
         """midnight job"""
-        
+
         seconds_until_send = outgoing_time - maya.now()
-        logging.info(
-            "sleeping %s seconds until message sends", seconds_until_send
-        )
+        logging.info("sleeping %s seconds until message sends", seconds_until_send)
         await asyncio.sleep(seconds_until_send.seconds)
-        await self.send_message(recipient,outgoing_message)
-    
-    
+        await self.send_message(recipient, outgoing_message)
+
+    def parse_schedule(self, message: Message) -> tuple[maya.MayaDT,str]:
+        """Parse a message of the form schedule "yyyy-mm-dd HH:MM" "message" """
+
+        try:
+            outgoing_time = maya.when(message.arg1)
+        except ValueError:
+            logging.info("couldn't get time automatically")
+
+        outgoing_message = message.arg2
+
+        return outgoing_time, outgoing_message
+
     async def do_schedule(self, message: Message) -> str:
         """
         Schedule a message to be sent at a specific later date.
@@ -42,12 +49,28 @@ class ScheduleBot(QuestionBot):
         # outgoing_message = self.ask_freeform_question(message.source, prompt)
         # if not (self.ask_yesno_question(message.source,f"You want me to send: \n {outgoing_message} is that correct? y/n")):
         #     return "oh ok, start over then"
-        
+
         # prompt = "ok now let's figure out WHEN to send your message. Please provide a future time in the form of yyyy/mm/dd HH:MM Timezone"
         # outgoing_message = message.arg1
 
-        if not isinstance(message.arg1,str):
-            time_info = await self.ask_freeform_question(message.source, "When do you want me to send your message?")
+        outgoing_time,outgoing_message = self.parse_schedule(message)
+        time_until = outgoing_time - maya.now()
+
+        if await self.ask_yesno_question(
+            message.source,
+            f"Ok we'll send \n {outgoing_message} \n in {time_until.seconds} seconds. \n Is that Ok? do you like this, am I a gfood bot y/n",
+        ):
+            asyncio.create_task(
+                self.schedule_send_message(
+                    message.source, outgoing_message, outgoing_time
+                )
+            )
+            return "you got it boss"
+
+        if not isinstance(message.arg1, str):
+            time_info = await self.ask_freeform_question(
+                message.source, "When do you want me to send your message?"
+            )
         else:
             time_info = message.text
         try:
@@ -55,20 +78,18 @@ class ScheduleBot(QuestionBot):
         except ValueError:
             return "Couldn't understand that time. Try yyyy-mm-dd HH:MM TZ"
 
-        outgoing_message = await self.ask_freeform_question(message.source,"What do you want your message to say?")
+        outgoing_message = await self.ask_freeform_question(
+            message.source, "What do you want your message to say?"
+        )
 
-        time_until = outgoing_time-maya.now()
+        time_until = outgoing_time - maya.now()
 
-        asyncio.create_task(self.schedule_send_message(message.source,outgoing_message,outgoing_time))
+        asyncio.create_task(
+            self.schedule_send_message(message.source, outgoing_message, outgoing_time)
+        )
 
-
-        return f"Ok, we'll send \n\"{outgoing_message}\" \n for you in {time_until.seconds} seconds"
-        
+        return f'Ok, we\'ll send \n"{outgoing_message}" \n for you in {time_until.seconds} seconds at {outgoing_time.datetime()}'
         # timestamp.datetime().strftime("%m/%d/%Y, %H:%M:%S %Z")
-
-
-
-
 
 
 if __name__ == "__main__":
