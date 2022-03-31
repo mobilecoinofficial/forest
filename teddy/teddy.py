726c45f4e5f9c712a19236a46e6bd85ea8a445c5
technillogue <technillogue@gmail.com>
2022-03-31 17:34:27 -0400

    msg/message

--- teddy/teddy.py
+++ teddy/teddy.py
@@ -183,14 +183,14 @@ class Teddy(DialogBot):
             )
             # retry send up to 3x for overkill
             for _ in range(3):
-                await self.send_typing(message)
+                await self.send_typing(msg)
                 payment_result = await self.pay_user_from_balance(user, "teddy", 4)
                 if payment_result and "Error" not in payment_result:
                     await self.send_message(
                         user,
                         await self.dialog.get("JUST_SENT_PAYMENT", "JUST_SENT_PAYMENT"),
                     )
-                    await self.send_typing(message, stop=True)
+                    await self.send_typing(msg, stop=True)
                     break
                 await self.send_message(
                     user,
