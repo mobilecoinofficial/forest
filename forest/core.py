edbb887681847ee70ad2e6c1c22805820695656f
technillogue <technillogue@gmail.com>
2022-04-01 17:52:32 -0400

    harmonize types

--- forest/core.py
+++ forest/core.py
@@ -378,7 +378,7 @@ class Signal:
         endsession: bool = False,
         attachments: Optional[list[str]] = None,
         content: str = "",
-        **other_params: str,
+        **other_params: Any,
     ) -> str:
         """
         Builds send command for the specified recipient in jsonrpc format and
@@ -460,22 +460,26 @@ class Signal:
         await self.outbox.put(json_command)
         return rpc_id
 
-    async def admin(self, msg: Response, **kwargs: Any) -> None:
+    async def admin(self, msg: Response, **other_params: Any) -> None:
         "send a message to admin"
         if (group := utils.get_secret("ADMIN_GROUP")) and not utils.AUXIN:
-            await self.send_message(None, msg, group=group, **kwargs)
+            await self.send_message(None, msg, group=group, **other_params)
         else:
-            await self.send_message(utils.get_secret("ADMIN"), msg, **kwargs)
+            await self.send_message(utils.get_secret("ADMIN"), msg, **other_params)
 
-    async def respond(self, target_msg: Message, msg: Response, **kwargs: Any) -> str:
+    async def respond(
+        self, target_msg: Message, msg: Response, **other_params: Any
+    ) -> str:
         """Respond to a message depending on whether it's a DM or group"""
         logging.debug("responding to %s", target_msg.source)
         if not target_msg.source:
             logging.error(json.dumps(target_msg.blob))
         if not utils.AUXIN and target_msg.group:
-            return await self.send_message(None, msg, group=target_msg.group, **kwargs)
+            return await self.send_message(
+                None, msg, group=target_msg.group, **other_params
+            )
         destination = target_msg.source or target_msg.uuid
-        return await self.send_message(destination, msg, **kwargs)
+        return await self.send_message(destination, msg, **other_params)
 
     async def send_reaction(self, target_msg: Message, emoji: str) -> None:
         """Send a reaction. Protip: you can use e.g. \N{GRINNING FACE} in python"""
