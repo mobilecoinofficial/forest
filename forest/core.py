eedb5b9116cae564eb23a9a6d2fa9ee49ee95ab9
technillogue <technillogue@gmail.com>
2022-04-29 14:59:15 -0700

    auxin supports groups now (#190)
    * auxin supports groups now
    
    * refactor into check_valid_recipient

--- forest/core.py
+++ forest/core.py
@@ -86,6 +86,17 @@ def rpc(
     }
 
 
+def check_valid_recipient(recipient: str) -> bool:
+    try:
+        assert recipient == utils.signal_format(recipient)
+    except (AssertionError, NumberParseException):
+        try:
+            assert recipient == str(uuid.UUID(recipient))
+        except (AssertionError, ValueError):
+            return False
+    return True
+
+
 async def get_attachment_paths(message: Message) -> list[str]:
     if not utils.AUXIN:
         return [
