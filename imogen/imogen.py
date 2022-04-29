87b769fc5d30cb5e5d8de8cdd4cd98cfa458eede
technillogue <technillogue@gmail.com>
2022-04-29 17:43:51 -0400

    add selector to enqueue_prompt procedure and have diffusion use that

--- imogen/imogen.py
+++ imogen/imogen.py
@@ -41,6 +41,7 @@ class InsertedPrompt:
     group: str
     params: dict
     url: str = utils.URL
+    selector: str = ""
 
     def as_args(self) -> list:
         return [
@@ -50,6 +51,7 @@ class InsertedPrompt:
             self.group,
             json.dumps(self.params),
             self.url,
+            self.selector,
         ]
 
 
@@ -78,7 +80,7 @@ QueueExpressions = pghelp.PGExpressions(
         sent_ts BIGINT DEFAULT null,
         errors INTEGER DEFAULT 0,
         selector TEXT);""",
-    enqueue_any="SELECT enqueue_prompt(prompt:=$1, _author:=$2, signal_ts:=$3, group_id:=$4, params:=$5, url:=$6)",
+    enqueue_any="SELECT enqueue_prompt(prompt:=$1, _author:=$2, signal_ts:=$3, group_id:=$4, params:=$5, url:=$6, selector:=$7)",
     enqueue_free="SELECT enqueue_free_prompt(prompt:=$1, _author:=$2, signal_ts:=$3, group_id:=$4, params:=$5, url:=$6)",
     enqueue_paid="SELECT enqueue_paid_prompt(prompt:=$1, author:=$2, signal_ts:=$3, group_id:=$4, params:=$5, url:=$6)",
     length="SELECT count(id) AS len FROM {self.table} WHERE status='pending' OR status='assigned';",
@@ -497,15 +499,12 @@ class Imogen(GelatoBot):
         return "sorry, that didn't work"
 
     async def enqueue_prompt(
-        self,
-        msg: Message,
-        params: dict,
-        attachments: str = "",
+        self, msg: Message, params: dict, attachments: str = "", selector: str = ""
     ) -> str:
         if attachments != "target" and not msg.text.strip():
             return "a prompt is required"
         logging.info(msg.full_text)
-        if "porn" in msg.text:
+        if any(bad in msg.text for bad in ["porn", "rape", "orgy", "sex"]):
             return "no"
         if attachments == "init":
             params.update(await self.upload_attachment(msg))
@@ -519,6 +518,7 @@ class Imogen(GelatoBot):
             signal_ts=msg.timestamp,
             group=msg.group or "",
             params=params,
+            selector=selector,
         )
         result = (await self.queue.enqueue_any(*prompt.as_args()))[0].get(
             "enqueue_prompt"
@@ -547,8 +547,8 @@ class Imogen(GelatoBot):
     async def do_imagine(self, msg: Message) -> str:
         """
         /imagine [prompt]
-        Generates an image based on your prompt.
-        Request is handled in the free queue, every free request is addressed and generated sequentially.
+         Generates an image based on your prompt.
+         Request is handled in the free queue, every free request is addressed and generated sequentially.
         """
         return await self.enqueue_prompt(msg, {}, attachments="init")
 
@@ -641,25 +641,28 @@ class Imogen(GelatoBot):
         if not msg.text.strip():
             return "a prompt is required"
         logging.info(msg.full_text)
-        params = {} if msg.group else {"nopost": True}
-        ret = await self.queue.execute(
-            """INSERT INTO prompt_queue (prompt, author, group_id, signal_ts, url, params, selector, paid)
-            VALUES ($1, $2, $3, $4, $5, $6, 'diffuse', false)
-            RETURNING (SELECT count(*) + 1 FROM prompt_queue WHERE
-            selector='diffuse' AND (status='pending' OR status='assigned')) as queue_length;""",
-            msg.text,
-            msg.source,
-            msg.group,
-            msg.timestamp,
-            utils.URL,
-            json.dumps(params),
+        if any(bad in msg.text for bad in ["porn", "rape", "orgy", "sex"]):
+            return "no"
+        params = {}
+        if not msg.group:
+            params["nopost"] = True
+        prompt = InsertedPrompt(
+            prompt=msg.text,
+            author=msg.source,
+            signal_ts=msg.timestamp,
+            group=msg.group or "",
+            params=params,
+            selector="diffuse",
         )
-        if not ret:
-            return "sorry, something wrong happened"
-        logging.info(ret)
+        result = (await self.queue.enqueue_any(*prompt.as_args()))[0].get(
+            "enqueue_prompt"
+        )
+        logging.info(result)
+        if not result.get("success"):
+            return dedent(messages["rate_limit"]).strip()
         worker_created = await self.ensure_unique_worker("diffuse.yaml")
         deets = " (started a new worker)" if worker_created else ""
-        return f"you are #{ret[0]['queue_length']} in the diffusion line{deets}"
+        return f"you are #{result['queue_length']} in the diffusion line{deets}"
 
     def make_prefix(prefix: str) -> Callable:  # type: ignore  # pylint: disable=no-self-argument
         async def wrapped(self: "Imogen", msg: Message) -> Response:
--- imogen/pl_imogen.sql
+++ imogen/pl_imogen.sql
@@ -18,15 +18,15 @@ CREATE TYPE enqueue_result AS (
     workers integer
 );
 
-CREATE OR REPLACE FUNCTION enqueue_prompt(prompt TEXT, _author TEXT, signal_ts BIGINT, group_id TEXT, params TEXT, url TEXT)
+CREATE OR REPLACE FUNCTION enqueue_prompt(prompt TEXT, _author TEXT, signal_ts BIGINT, group_id TEXT, params TEXT, url TEXT, selector TEXT default '')
 RETURNS enqueue_result AS $$ 
     DECLARE
         result enqueue_result;
         prompt_id INTEGER;
     BEGIN
         IF get_balance(_author) >= 0.10 THEN
-            INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url)
-                VALUES (prompt, true, _author, signal_ts, group_id, params, url)  RETURNING id INTO prompt_id;
+            INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url, selector)
+                VALUES (prompt, true, _author, signal_ts, group_id, params, url, selector)  RETURNING id INTO prompt_id;
             INSERT INTO imogen_ledger (account, amount_usd_cents, memo, ts) 
                 VALUES(_author, -10, prompt_id::text, CURRENT_TIMESTAMP);
             SELECT true, true, get_balance(_author) >= 0.10 INTO result.success, result.paid, result.balance_remaining;
@@ -35,8 +35,8 @@ RETURNS enqueue_result AS $$
                 WHERE (status='pending' OR status='assigned') AND paid=true 
                 INTO result.queue_length;
         ELSEIF (SELECT count (id) <= 5 FROM prompt_queue WHERE author=_author AND (status='pending' OR status='assigned') AND paid=false) THEN
-            INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url)
-                VALUES (prompt, false, _author, signal_ts, group_id, params, url);
+            INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url, selector)
+                VALUES (prompt, false, _author, signal_ts, group_id, params, url, selector);
             SELECT true, false, false INTO result.success, result.paid, result.balance_remaining;
             SELECT coalesce(count(distinct hostname), 0) FROM prompt_queue WHERE status='assigned' AND paid=false INTO result.workers;
             SELECT count(*) FROM prompt_queue 
@@ -49,48 +49,48 @@ RETURNS enqueue_result AS $$
     END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION enqueue_paid_prompt(prompt TEXT, author TEXT, signal_ts BIGINT, group_id TEXT, params TEXT, url TEXT)
-RETURNS enqueue_result AS $$ 
-    DECLARE
-        result enqueue_result;
-        prompt_id INTEGER;
-    BEGIN
-        IF get_balance(author) < 0.10 THEN
-            SELECT false, true, false INTO result.success, result.paid, result.balance_remaining;
-        ELSE 
-            INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url)
-                VALUES (prompt, true, author, signal_ts, group_id, params, url)  RETURNING id INTO prompt_id;
-            INSERT INTO imogen_ledger (account, amount_usd_cents, memo, ts) 
-                VALUES(author, -10, prompt_id::text, CURRENT_TIMESTAMP);
-            SELECT true, true, get_balance(author) >= 0.10 INTO result.success, result.paid, result.balance_remaining;
-        END IF;
-        SELECT coalesce(count(distinct hostname), 0) FROM prompt_queue WHERE status='assigned' INTO result.workers;
-        SELECT count(*) FROM prompt_queue 
-            WHERE (status='pending' OR status='assigned') AND paid=true 
-            INTO result.queue_length;
-        RETURN result;
-    END;
-$$ LANGUAGE plpgsql;
+# CREATE OR REPLACE FUNCTION enqueue_paid_prompt(prompt TEXT, author TEXT, signal_ts BIGINT, group_id TEXT, params TEXT, url TEXT)
+# RETURNS enqueue_result AS $$ 
+#     DECLARE
+#         result enqueue_result;
+#         prompt_id INTEGER;
+#     BEGIN
+#         IF get_balance(author) < 0.10 THEN
+#             SELECT false, true, false INTO result.success, result.paid, result.balance_remaining;
+#         ELSE 
+#             INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url)
+#                 VALUES (prompt, true, author, signal_ts, group_id, params, url)  RETURNING id INTO prompt_id;
+#             INSERT INTO imogen_ledger (account, amount_usd_cents, memo, ts) 
+#                 VALUES(author, -10, prompt_id::text, CURRENT_TIMESTAMP);
+#             SELECT true, true, get_balance(author) >= 0.10 INTO result.success, result.paid, result.balance_remaining;
+#         END IF;
+#         SELECT coalesce(count(distinct hostname), 0) FROM prompt_queue WHERE status='assigned' INTO result.workers;
+#         SELECT count(*) FROM prompt_queue 
+#             WHERE (status='pending' OR status='assigned') AND paid=true 
+#             INTO result.queue_length;
+#         RETURN result;
+#     END;
+# $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION enqueue_free_prompt(prompt TEXT, _author TEXT, signal_ts BIGINT, group_id TEXT, params TEXT, url TEXT)
-RETURNS enqueue_result AS $$ 
-    DECLARE
-        result enqueue_result;
-    BEGIN
-        IF (SELECT count (id) > 5 FROM prompt_queue WHERE author=_author AND status='pending' AND paid=false) THEN
-            SELECT false, false INTO result.success, result.paid;
-        ELSE 
-            INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url)
-                VALUES (prompt, false, _author, signal_ts, group_id, params, url);
-            SELECT true, false INTO result.success, result.paid;
-        END IF;
-        SELECT coalesce(count(distinct hostname), 0) FROM prompt_queue WHERE status='assigned' INTO result.workers;
-        SELECT count(*) FROM prompt_queue 
-            WHERE (status='pending' OR status='assigned') AND paid=False
-            INTO result.queue_length;
-        RETURN result;
-    END;
-$$ LANGUAGE plpgsql;
+# CREATE OR REPLACE FUNCTION enqueue_free_prompt(prompt TEXT, _author TEXT, signal_ts BIGINT, group_id TEXT, params TEXT, url TEXT)
+# RETURNS enqueue_result AS $$ 
+#     DECLARE
+#         result enqueue_result;
+#     BEGIN
+#         IF (SELECT count (id) > 5 FROM prompt_queue WHERE author=_author AND status='pending' AND paid=false) THEN
+#             SELECT false, false INTO result.success, result.paid;
+#         ELSE 
+#             INSERT INTO prompt_queue (prompt, paid, author, signal_ts, group_id, params, url)
+#                 VALUES (prompt, false, _author, signal_ts, group_id, params, url);
+#             SELECT true, false INTO result.success, result.paid;
+#         END IF;
+#         SELECT coalesce(count(distinct hostname), 0) FROM prompt_queue WHERE status='assigned' INTO result.workers;
+#         SELECT count(*) FROM prompt_queue 
+#             WHERE (status='pending' OR status='assigned') AND paid=False
+#             INTO result.queue_length;
+#         RETURN result;
+#     END;
+# $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION get_prompt(paid BOOLEAN, claimant TEXT) RETURNS record AS $$
     DECLARE
